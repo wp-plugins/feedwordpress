a4e93bb6bc21f22d9015c42e11f97b3e699ce360
radgeek <radgeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-12 21:23:17 +0000

    WP 3.5 and PHP 5.4 compat audits.
    git-svn-id: https://plugins.svn.wordpress.org/feedwordpress/trunk@638053 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- feedwordpress.php
+++ feedwordpress.php
@@ -187,6 +187,7 @@ if (!FeedWordPress::needs_upgrade()) : // only work if the conditions are safe!
 	remove_filter('pre_link_url', 'wp_filter_kses');
 	
 	# Admin menu
+	add_action('admin_init', array('feedwordpress', 'admin_init'));
 	add_action('admin_menu', 'fwp_add_pages');
 	add_action('admin_notices', 'fwp_check_debug');
 
@@ -218,12 +219,12 @@ if (!FeedWordPress::needs_upgrade()) : // only work if the conditions are safe!
 	# Cron-less auto-update. Hooray!
 	$autoUpdateHook = $feedwordpress->automatic_update_hook();
 	if (!is_null($autoUpdateHook)) :
-		add_action($autoUpdateHook, array(&$feedwordpress, 'auto_update'));
+		add_action($autoUpdateHook, array($feedwordpress, 'auto_update'));
 	endif;
 	
-	add_action('init', array(&$feedwordpress, 'init'));
-	add_action('shutdown', array(&$feedwordpress, 'email_diagnostic_log'));
-	add_action('wp_dashboard_setup', array(&$feedwordpress, 'dashboard_setup'));
+	add_action('init', array($feedwordpress, 'init'));
+	add_action('shutdown', array($feedwordpress, 'email_diagnostic_log'));
+	add_action('wp_dashboard_setup', array($feedwordpress, 'dashboard_setup'));
 
 	# Default sanitizers
 	add_filter('syndicated_item_content', array('SyndicatedPost', 'resolve_relative_uris'), 0, 2);
@@ -671,6 +672,12 @@ function fwp_add_pages () {
 		$syndicationMenu,
 		NULL,
 		WP_PLUGIN_URL.'/'.FeedWordPress::path('feedwordpress-tiny.png')
+	);
+
+	do_action('feedwordpress_admin_menu_pre_feeds', $menu_cap, $settings_cap);
+	add_submenu_page(
+		$syndicationMenu, 'Syndicated Sites', 'Syndicated Sites',
+		$settings_cap, $syndicationMenu
 	);
 
 	do_action('feedwordpress_admin_menu_pre_feeds', $menu_cap, $settings_cap);
@@ -1193,6 +1200,15 @@ class FeedWordPress {
 		return $ret;
 	} // FeedWordPress::stale()
 
+	static function admin_init () {
+		// WordPress 3.5+ compat: the WP devs are in the midst of removing Links from the WordPress core. Eventually we'll have to deal
+		// with the possible disappearance of the wp_links table as a whole; but in the meantime, we just need to turn on the interface
+		// element to avoid problems with user capabilities that presume the presence of the Links Manager in the admin UI.
+		if (!intval(get_option('link_manager_enabled', false))) :
+			update_option('link_manager_enabled', true);
+		endif;
+	} /* FeedWordPress::admin_init() */
+
 	function init () {
 		global $fwp_path;
 		
@@ -1227,7 +1243,7 @@ class FeedWordPress {
 		));
 		add_action(
 			/*hook=*/ 'template_redirect',
-			/*function=*/ array(&$this, 'redirect_retired'),
+			/*function=*/ array($this, 'redirect_retired'),
 			/*priority=*/ -100
 		);
 
@@ -1272,12 +1288,12 @@ class FeedWordPress {
 			add_meta_box(
 				/*id=*/ $widget_id,
 				/*title=*/ $widget_name,
-				/*callback=*/ array(&$this, 'dashboard'),
+				/*callback=*/ array($this, 'dashboard'),
 				/*page=*/ 'dashboard',
 				/*context=*/ $column,
 				/*priority=*/ $priority
 			);
-			/*control_callback= array(&$this, 'dashboard_control') */
+			/*control_callback= array($this, 'dashboard_control') */
 			
 			// This is kind of rude, I know, but the dashboard widget isn't
 			// worth much if users don't know that it exists, and I don't
